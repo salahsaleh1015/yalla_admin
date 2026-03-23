986c43a554c8650722b98614efbf2a4214a0d62c
Salah Saleh <salahsaleh1015@gmail.com>
2026-03-24 00:53:26 +0200

    add products methods in firebase services class

--- lib/core/services/firebase_firestore_services/firestore_home_services.dart
+++ lib/core/services/firebase_firestore_services/firestore_home_services.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:yalla_admin/data/models/banner_model.dart';
+import 'package:yalla_admin/data/models/product_model.dart';
 import 'package:yalla_admin/data/models/shop_model.dart';
 
 class FirestoreHomeServices {
@@ -45,7 +46,6 @@ class FirestoreHomeServices {
   }
 
   Future<void> addShopToFireStore(ShopModel shop) async {
-
     final docRef = _shopsCollectionRef.doc();
 
     final model = ShopModel(
@@ -53,9 +53,53 @@ class FirestoreHomeServices {
       image: shop.shopImage,
       name: shop.shopName,
       address: shop.shopAddress,
-      phoneNumber: shop.shopPhoneNumber
+      phoneNumber: shop.shopPhoneNumber,
     );
 
     await docRef.set(model.toJson());
+  }
+
+  Future<void> addShopProductToFireStore({
+    required String shopId,
+    required ProductModel product,
+  }) async {
+    // بنوصل للـ Document بتاع المحل وبعدين نفتح Sub-collection باسم products
+    final docRef = _shopsProductsCollectionRef(shopId).doc();
+
+    final model = ProductModel(
+      id: docRef.id, // بنستخدم الـ ID التلقائي اللي Firestore عمله
+      name: product.name,
+      price: product.price,
+      description: product.description,
+      image: product.image,
+    );
+
+    await docRef.set(model.toJson());
+  }
+
+  Future<void> editProduct({
+    required String shopId,
+    required String productId,
+    required ProductModel updatedProduct,
+  }) async {
+    try {
+      await _shopsProductsCollectionRef(
+        shopId,
+      ).doc(productId).update(updatedProduct.toJson());
+      // ملحوظة: تأكد إن toJson() مش بترجع الـ ID لو مش عايز تخزنه مرتين
+    } catch (e) {
+      throw Exception("فشل تعديل المنتج: $e");
+    }
+  }
+
+  Future<void> deleteProduct({
+    required String shopId,
+    required String productId,
+  }) async {
+    try {
+      await _shopsProductsCollectionRef(shopId).doc(productId).delete();
+    } catch (e) {
+      throw Exception("فشل حذف المنتج: $e");
+    }
   }
 }
